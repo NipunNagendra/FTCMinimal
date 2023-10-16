aee4f527b5e03790a011c7795f5cdc355b1a19dc
Your Name <nipun.nagendra9@gmail.com>
2023-10-16 17:04:45 -0500

    Made 33 teleops, main, test, and og

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestTeleOp.java
@@ -0,0 +1,123 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import android.graphics.Color;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="TestTeleOp", group="TeleOp")
+public class TestTeleOp extends OpMode {
+
+    Movement move;
+    RevColorSensorV3 cs;
+    DistanceSensor ds;
+    String moveType = "robot";
+
+    double[] motorPower = {0, 0, 0, 0};
+
+    public void init() {
+        move = new Movement(hardwareMap);
+        cs = hardwareMap.get(RevColorSensorV3.class, "cs");
+        ds = hardwareMap.get(DistanceSensor.class, "ds");
+        telemetry.addData("init", "completed");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop()
+    {
+        //imu for field oriented drive
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT
+        ));
+
+        double leftY;
+        double leftX;
+        double rightX;
+
+
+        int red = cs.red();
+        int green = cs.green();
+        int blue = cs.blue();
+
+        float[] hsv = new float[3];
+        Color.RGBToHSV(red,green,blue,hsv);
+        float hue = hsv[0];
+        String color;
+
+        if(hue>=190 && hue<=300){
+            color="purple";
+        }
+        else if(hue>=105 && hue<=150 ){
+            color="green";
+        }
+        else if(hue>=16 && hue<=100){
+            color="yellow";
+        }
+        else{
+            color="weird color";
+        }
+
+        telemetry.addData("Red: ",cs.red());
+        telemetry.addData("Green: ",cs.green());
+        telemetry.addData("Blue: ",cs.blue());
+        telemetry.addData("Hue", hue);
+        telemetry.addLine(color);
+        telemetry.addData("Distance(Cm)", ds.getDistance(DistanceUnit.CM));
+        telemetry.addLine(moveType);
+        telemetry.update();
+
+        if(ds.getDistance(DistanceUnit.CM)>=35 && ds.getDistance(DistanceUnit.CM)<=45){
+            gamepad1.rumble(1,1,100);
+        }
+
+        if (gamepad1.options) {
+            imu.resetYaw();
+        }
+
+        if (move.isPressed("share", gamepad1.share)) {
+            if (moveType == "robot") {
+                moveType = "field";
+            }
+            else if(moveType == "field"){
+                moveType = "robot";
+            }
+        }
+
+        if (Math.abs(gamepad1.left_stick_y)  > 0.1 ||
+                Math.abs(gamepad1.left_stick_x)  > 0.1 ||
+                Math.abs(gamepad1.right_stick_x) > 0.1) {
+
+            leftY = gamepad1.left_stick_y;
+            leftX = -1*(gamepad1.left_stick_x);
+            rightX = -1*(gamepad1.right_stick_x);
+
+            if(moveType=="robot") {
+                motorPower = move.holonomicDrive(leftX, leftY, rightX);
+            }
+            else if(moveType=="field"){
+                motorPower = move.fieldDrive(leftX,leftY, rightX, imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+            }
+        }
+        else {
+            if(moveType=="robot") {
+                motorPower = move.holonomicDrive(0, 0, 0);
+            }
+            else if(moveType=="field"){
+                motorPower = move.fieldDrive(0,0, 0, imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+            }
+        }
+
+        move.setPowers(motorPower[0], motorPower[1], motorPower[2], motorPower[3]);
+    }
+
+}
