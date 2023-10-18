54e78c7468b2bc1b276ebf5a6baad9c90fa644c9
Sabrina M <sabrina.mohanty@gmail.com>
2023-10-18 17:32:42 -0500

    New openCV detection file found on FTC Discord

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CameraTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CameraTester.java
@@ -22,7 +22,7 @@ import org.openftc.easyopencv.OpenCvPipeline;
 import java.util.ArrayList;
 import java.util.List;
 
-@TeleOp(name = "OpenCV Testing")
+@TeleOp(name = "Camera Testing")
 
 public class CameraTester extends LinearOpMode {
 
@@ -35,8 +35,8 @@ public class CameraTester extends LinearOpMode {
     private static final int CAMERA_HEIGHT = 360; // height of wanted camera resolution
 
     // Calculate the distance using the formula
-    public static final double objectWidthInRealWorldUnits = 3.75;  // Replace with the actual width of the object in real-world units
-    public static final double focalLength = 728;  // Replace with the focal length of the camera in pixels
+//    public static final double objectWidthInRealWorldUnits = 3.75;  // Replace with the actual width of the object in real-world units
+//    public static final double focalLength = 728;  // Replace with the focal length of the camera in pixels
 
 
     @Override
@@ -52,7 +52,7 @@ public class CameraTester extends LinearOpMode {
 
         while (opModeIsActive()) {
             telemetry.addData("Coordinate", "(" + (int) cX + ", " + (int) cY + ")");
-            telemetry.addData("Distance in Inch", (getDistance(width)));
+//            telemetry.addData("Distance in Inch", (getDistance(width)));
             //telemetry.addData("RGB Value: ");
             telemetry.update();
 
@@ -73,94 +73,99 @@ public class CameraTester extends LinearOpMode {
         controlHubCam = OpenCvCameraFactory.getInstance().createWebcam(
                 hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
-        controlHubCam.setPipeline(new YellowBlobDetectionPipeline());
+//        controlHubCam.setPipeline(new YellowBlobDetectionPipeline());
 
         controlHubCam.openCameraDevice();
         controlHubCam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
     }
-    class YellowBlobDetectionPipeline extends OpenCvPipeline {
-        @Override
-        public Mat processFrame(Mat input) {
+//    class YellowBlobDetectionPipeline extends OpenCvPipeline {
+//        @Override
+//        public Mat processFrame(Mat input) {
             // Preprocess the frame to detect yellow regions
-            Mat yellowMask = preprocessFrame(input);
+//            Mat yellowMask = preprocessFrame(input);
 
             // Find contours of the detected yellow regions
-            List<MatOfPoint> contours = new ArrayList<>();
-            Mat hierarchy = new Mat();
-            Imgproc.findContours(yellowMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+//            List<MatOfPoint> contours = new ArrayList<>();
+//            Mat hierarchy = new Mat();
+//            Imgproc.findContours(yellowMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
             // Find the largest yellow contour (blob)
-            MatOfPoint largestContour = findLargestContour(contours);
+//            MatOfPoint largestContour = findLargestContour(contours);
 
-            if (largestContour != null) {
+//            if (largestContour != null) {
                 // Draw a red outline around the largest detected object
-                Imgproc.drawContours(input, contours, contours.indexOf(largestContour), new Scalar(255, 0, 0), 2);
+//                Imgproc.drawContours(input, contours, contours.indexOf(largestContour), new Scalar(255, 0, 0), 2);
                 // Calculate the width of the bounding box
-                width = calculateWidth(largestContour);
+//                width = calculateWidth(largestContour);
 
                 // Display the width next to the label
-                String widthLabel = "Width: " + (int) width + " pixels";
-                Imgproc.putText(input, widthLabel, new Point(cX + 10, cY + 20), Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0), 2);
+//                String widthLabel = "Width: " + (int) width + " pixels";
+//                Imgproc.putText(input, widthLabel, new Point(cX + 10, cY + 20), Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0), 2);
                 //Display the Distance
-                String distanceLabel = "Distance: " + String.format("%.2f", getDistance(width)) + " inches";
-                Imgproc.putText(input, distanceLabel, new Point(cX + 10, cY + 60), Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0), 2);
+//                String distanceLabel = "Distance: " + String.format("%.2f", getDistance(width)) + " inches";
+//                Imgproc.putText(input, distanceLabel, new Point(cX + 10, cY + 60), Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0), 2);
                 // Calculate the centroid of the largest contour
-                Moments moments = Imgproc.moments(largestContour);
-                cX = moments.get_m10() / moments.get_m00();
-                cY = moments.get_m01() / moments.get_m00();
+//                Moments moments = Imgproc.moments(largestContour);
+//                cX = moments.get_m10() / moments.get_m00();
+//                cY = moments.get_m01() / moments.get_m00();
 
                 // Draw a dot at the centroid
-                String label = "(" + (int) cX + ", " + (int) cY + ")";
-                Imgproc.putText(input, label, new Point(cX + 10, cY), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0, 255, 0), 2);
-                Imgproc.circle(input, new Point(cX, cY), 5, new Scalar(0, 255, 0), -1);
+//                String label = "(" + (int) cX + ", " + (int) cY + ")";
+//                Imgproc.putText(input, label, new Point(cX + 10, cY), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0, 255, 0), 2);
+//                Imgproc.circle(input, new Point(cX, cY), 5, new Scalar(0, 255, 0), -1);
+//
+//            }
 
-            }
+//            return input;
+//        }
 
-            return input;
-        }
+//        private Mat preprocessFrame(Mat frame) {
+//            Mat hsvFrame = new Mat();
+//            Imgproc.cvtColor(frame, hsvFrame, Imgproc.COLOR_BGR2HSV);
+//
+//            Scalar lowerYellow = new Scalar(100, 100, 100);
+//            Scalar upperYellow = new Scalar(180, 255, 255);
+//
+//
+//            Mat yellowMask = new Mat();
+//            Core.inRange(hsvFrame, lowerYellow, upperYellow, yellowMask);
+//
+//            Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
+//            Imgproc.morphologyEx(yellowMask, yellowMask, Imgproc.MORPH_OPEN, kernel);
+//            Imgproc.morphologyEx(yellowMask, yellowMask, Imgproc.MORPH_CLOSE, kernel);
+//
+//            return yellowMask;
+//        }
 
-        private Mat preprocessFrame(Mat frame) {
-            Mat hsvFrame = new Mat();
-            Imgproc.cvtColor(frame, hsvFrame, Imgproc.COLOR_BGR2HSV);
+//        private MatOfPoint findLargestContour(List<MatOfPoint> contours) {
+//            double maxArea = 0;
+//            MatOfPoint largestContour = null;
+//
+//            for (MatOfPoint contour : contours) {
+//                double area = Imgproc.contourArea(contour);
+//                if (area > maxArea) {
+//                    maxArea = area;
+//                    largestContour = contour;
+//                }
+//            }
+//
+//            return largestContour;
+//        }
+//        private double calculateWidth(MatOfPoint contour) {
+//            Rect boundingRect = Imgproc.boundingRect(contour);
+//            return boundingRect.width;
+//        }
 
-            Scalar lowerYellow = new Scalar(100, 100, 100);
-            Scalar upperYellow = new Scalar(180, 255, 255);
+//    }
+//    private static double getDistance(double width){
+//        double distance = (objectWidthInRealWorldUnits * focalLength) / width;
+//        return distance;
+//    }
 
-
-            Mat yellowMask = new Mat();
-            Core.inRange(hsvFrame, lowerYellow, upperYellow, yellowMask);
-
-            Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
-            Imgproc.morphologyEx(yellowMask, yellowMask, Imgproc.MORPH_OPEN, kernel);
-            Imgproc.morphologyEx(yellowMask, yellowMask, Imgproc.MORPH_CLOSE, kernel);
-
-            return yellowMask;
-        }
-
-        private MatOfPoint findLargestContour(List<MatOfPoint> contours) {
-            double maxArea = 0;
-            MatOfPoint largestContour = null;
-
-            for (MatOfPoint contour : contours) {
-                double area = Imgproc.contourArea(contour);
-                if (area > maxArea) {
-                    maxArea = area;
-                    largestContour = contour;
-                }
-            }
-
-            return largestContour;
-        }
-        private double calculateWidth(MatOfPoint contour) {
-            Rect boundingRect = Imgproc.boundingRect(contour);
-            return boundingRect.width;
-        }
-
-    }
-    private static double getDistance(double width){
-        double distance = (objectWidthInRealWorldUnits * focalLength) / width;
-        return distance;
-    }
-
+//    public String runningOpenCV() {
+//        OpenCVDetection opcv = new OpenCVDetection();
+//
+//        opvc.processFrame();
+//    }
 
 }
