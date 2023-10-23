68304f99a613e498caebbe6163ec578850c67991
Your Name <nipun.nagendra9@gmail.com>
2023-10-22 20:30:52 -0500

    Drivers worked on and guardmode improved

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/MB1242.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/MB1242.java
@@ -4,12 +4,28 @@ import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynchDevice;
 import com.qualcomm.robotcore.hardware.configuration.annotations.DeviceProperties;
 import com.qualcomm.robotcore.hardware.configuration.annotations.I2cDeviceType;
+import com.qualcomm.robotcore.util.TypeConversion;
 
 @I2cDeviceType
 @DeviceProperties(name = "MB1242 Ultrasonic Sensor", xmlTag = "MB1242")
  public class MB1242 extends I2cDeviceSynchDevice<I2cDeviceSynch>{
+    protected void writeShort(final Register reg, short value) {
+        deviceClient.write(reg.bVal, TypeConversion.shortToByteArray(value));
+    }
+
+    protected short readShort(Register reg) {
+        return TypeConversion.byteArrayToShort(deviceClient.read(reg.bVal, 2));
+    }
     public enum Register{
-        TAKE_RANGE()
+        INITIATE_WRITE(0xE0),
+        WRITE_RANGE(0x51),
+        INITIATE_READ(0xE1);
+
+        public int bVal;
+
+        Register(int bVal){
+            this.bVal=bVal;
+        }
     }
     @Override
     public Manufacturer getManufacturer(){
@@ -25,13 +41,34 @@ import com.qualcomm.robotcore.hardware.configuration.annotations.I2cDeviceType;
         return "Maxbotix MB1242 Ultrasonic Sensor";
     }
 
-    public MB1242(I2cDeviceSynch deviceClient, boolean deviceClientIsOwned) {
+
+
+    public MB1242(I2cDeviceSynch deviceClient, boolean deviceClientIsOwned)
+    {
         super(deviceClient, deviceClientIsOwned);
 
-        this.deviceClient.setI2cAddress(I2cAddr.create8bit(0x71));
+        this.setOptimalReadWindow();
+        this.deviceClient.setI2cAddress(I2cAddr.create7bit(0xE0));
 
         super.registerArmingStateCallback(false);
         this.deviceClient.engage();
+    }
+
+    protected void setOptimalReadWindow() {
+
+        I2cDeviceSynch.ReadWindow readWindow = new I2cDeviceSynch.ReadWindow(
+                Register.FIRST.bVal,
+                Register.LAST.bVal - Register.FIRST.bVal + 1,
+                I2cDeviceSynch.ReadMode.REPEAT);
+        this.deviceClient.setReadWindow(readWindow);
+    }
+
+    public short readRange(){
+        return readShort(Register.INITIATE_READ);
+    }
+
+    public short takeRange(){
+        
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/GuardMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/GuardMode.java
@@ -1,26 +1,39 @@
 package org.firstinspires.ftc.teamcode.testing;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.util.Angle;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
-public class GuardMode extends LinearOpMode {
+public class GuardMode extends OpMode {
     SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-    double xyParameter = 1;
-    double headingParameter = 1;
-    double lockThreshold = 0;
-    Pose2d lockLocation = new Pose2d(0, 0, 0);
+    double xyParameter;
+    double headingParameter;
+    double lockThreshold;
+    Pose2d lockLocation;
+    public void init() {
+        drive = new SampleMecanumDrive(hardwareMap);
+        xyParameter= 1;
+        headingParameter= 1;
+        lockThreshold=0;
+        lockLocation= new Pose2d(0,0,0);
+        telemetry.addData("init", "completed");
+    }
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        waitForStart();
-        while (opModeIsActive()) {
-            if((Math.abs(drive.getPoseEstimate().getX() - lockLocation.getX()) >= lockThreshold || Math.abs(drive.getPoseEstimate().getY() - lockLocation.getY()) >= lockThreshold)){
-                lockTo(lockLocation);
-            }
-            drive.update();
+    public void loop(){
+
+        if((Math.abs(drive.getPoseEstimate().getX() - lockLocation.getX()) >= lockThreshold
+                ||
+                Math.abs(drive.getPoseEstimate().getY() - lockLocation.getY()) >= lockThreshold)){
+            lockTo(lockLocation);
         }
+
+        drive.update();
     }
 
     public void lockTo(Pose2d targetPos) {
