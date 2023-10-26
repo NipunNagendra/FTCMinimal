8b7801e9e3a91f4db61845eb36afbf9117c44b03
Your Name <nipun.nagendra9@gmail.com>
2023-10-26 15:44:27 -0500

    CVrunner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/CVrunner.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import org.firstinspires.ftc.teamcode.testing.NipunCV;
+
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+
+import java.util.jar.Attributes;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="cvRunner-Nipun", group="TeleOp")
+public class CVrunner extends LinearOpMode {
+
+    private NipunCV.Location location;
+
+    OpenCvWebcam camera;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "identifyier","teamcode");
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        NipunCV detector = new NipunCV(telemetry);
+        camera.setPipeline(detector);
+        camera.setMillisecondsPermissionTimeout(5000);
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+
+                camera.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+
+
+            }
+        });
+
+        FtcDashboard.getInstance().startCameraStream(camera, 0);
+
+        waitForStart();
+
+        camera.stopStreaming();
+    }
+}
